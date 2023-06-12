8cef58be1231663e49e855369c8cfffad6919790
mod64 <mod64bits@gmail.com>
2023-06-12 15:30:31 -0300

    model clientes

--- apps/clientes/models.py
+++ apps/clientes/models.py
@@ -1,5 +1,16 @@
 from django.db import models
+from apps.core.models import BaseModel
 
 
-class Cliente(models.Model):
-    pass
+class Cliente(BaseModel):
+    nome = models.CharField('Cliente/Empresa', max_length=100)
+    documento = models.CharField('Documento', max_length=40, null=True, blank=True)
+    email = models.EmailField('E-mail', null=True, blank=True)
+    telefone = models.CharField('Telefone', max_length=20, null=True, blank=True)
+    endereco = models.CharField('Endereço', max_length=250, null=True, blank=True)
+    observacao = models.TextField('Observação', null=True, blank=True)
+    slug_from = 'nome'
+
+    def __str__(self):
+        return self.nome
+
