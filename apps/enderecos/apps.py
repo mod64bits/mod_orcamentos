343161b637f76fb831d2f8bbd54ff1a449cbe58f
mod64 <mod64bits@gmail.com>
2023-10-18 00:30:40 -0300

    modelagem enderecos

--- apps/enderecos/apps.py
+++ apps/enderecos/apps.py
@@ -4,3 +4,4 @@ from django.apps import AppConfig
 class EnderecosConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'apps.enderecos'
+    verbose_name = 'Gerenciamento Endereços'
