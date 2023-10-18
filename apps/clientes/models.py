61cb37bd99b8a921a1becdc7bb405b9eeb9e4403
mod64 <mod64bits@gmail.com>
2023-10-17 22:43:05 -0300

    modelagem clientes

--- apps/clientes/models.py
+++ apps/clientes/models.py
@@ -1,3 +1,25 @@
 from django.db import models
+from apps.base.models import Base
+from apps.accounts.models import User
 
-# Create your models here.
+
+class Clientes(Base):
+    TIPO_CLIENTE = [
+        ('normal', 'Normal'),
+        ('contrato', 'Contrato'),
+    ]
+    usuario = models.OneToOneField(
+        User,
+        on_delete=models.CASCADE,
+        related_name='client_user',
+        null=True,
+        blank=True
+    )
+    documento = models.CharField('Documento', max_length=50, null=True, blank=True)
+    whatsApp = models.CharField('WhatsApp', max_length=50)
+    tipo_cliente = models.CharField(
+        max_length=30, choices=TIPO_CLIENTE, default='normal')
+    bloqueado = models.BooleanField('Bloqueado', default=False)
+
+    def __str__(self):
+        return self.whatsApp
